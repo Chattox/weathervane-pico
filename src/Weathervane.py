0626165a9df8c1db22360086b0bebefb1eff5ffd
Chattox <conradlineker@gmail.com>
2024-08-02 12:24:37 +0100

    feat(weathervane): sleep method

--- src/Weathervane.py
+++ src/Weathervane.py
@@ -1,9 +1,12 @@
-from machine import Pin, RTC
+from machine import Pin, RTC, reset
+from time import sleep_ms
 from pimoroni_i2c import PimoroniI2C
 from pcf85063a import PCF85063A
 from wakeup import get_gpio_state
+from utils.config import reading_frequency
 from utils.constants import (
     BUTTON_PIN,
+    HOLD_VSYS_EN_PIN,
     I2C_SDA_PIN,
     I2C_SCL_PIN,
     RAIN_PIN,
@@ -28,7 +31,10 @@ class Weathervane:
     """
 
     def __init__(self):
+        # Hold VSYS_EN pin high to keep power to the board when on battery
+        self.__hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT, value=True)
         self.logger = Logging()
+        self.button = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_DOWN)
         # state of vbus to know if woken by USB
         self.__vbus_present = Pin("WL_GPIO2", Pin.IN).value()
         self.__i2c = PimoroniI2C(I2C_SDA_PIN, I2C_SCL_PIN, 100000)
@@ -77,3 +83,54 @@ class Weathervane:
             wake_reason = WAKE_USB_POWERED
 
         return wake_reason
+
+    def sleep(self):
+        """
+        Puts the enviro board into a sleep state by cutting power to the pico W.
+
+        If on USB power, this will have no effect and so the board will instead go
+        into a monitoring state awaiting the next trigger
+        """
+        self.logger.info("Going to sleep")
+
+        # Clear RTC flags
+        self.rtc.clear_alarm_flag()
+        self.rtc.clear_timer_flag()
+
+        # Set alarm for next scheduled reading
+        dt = self.rtc.datetime()
+        hour, minute, second = dt[3:6]
+
+        # Figure out what time the next reading should be at
+        minute += reading_frequency - (minute % reading_frequency)
+        if minute >= 60:
+            hour += 1
+            minute -= 60
+        if hour >= 24:
+            hour -= 24
+
+        self.logger.info(f"- Setting alarm to wake at {hour:02}:{minute:02}")
+
+        # Set RTC alarm
+        self.rtc.set_alarm(0, minute, hour)
+        self.rtc.enable_alarm_interrupt(True)
+
+        # Disable VSYS hold, cutting power to the pico (if on battery)
+        self.logger.info("- Shutting down (if on battery)")
+        self.__hold_vsys_en_pin.init(Pin.IN)
+
+        # If this code is reached it means power is coming from USB
+        # instead of battery so the board can't (and doesn't need to) go to sleep
+        self.activity_led.stop()
+
+        self.logger.info(
+            "- On USB power so can't shut down. Waiting for alarm or other trigger instead"
+        )
+        while not self.rtc.read_alarm_flag():
+            sleep_ms(50)
+            # Reset on button press
+            if self.button.value():
+                break
+
+        self.logger.info("- Button pressed, resetting board")
+        reset()
---
+++ src/utils/config_template.py
@@ -0,0 +1,4 @@
+# Template for config.py
+
+# How often to wake the board and take readings in minutes
+reading_frequency = 15
